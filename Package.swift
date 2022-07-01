b18b07911d646600bb28769e26f233682b0f5bd2
seb <sebastian@leantech.me>
2022-07-01 12:33:41 +0100

    SDK-303 fix: cdn url typo

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/1.4.0/LeanSDK.xcframework-1.5.0.zip", checksum: "fc78695379d66a654ed5687197a3709a00276c3cb6f3ae768b9bb9a2a71aa9b9")
+        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/1.5.0/LeanSDK.xcframework-1.5.0.zip", checksum: "fc78695379d66a654ed5687197a3709a00276c3cb6f3ae768b9bb9a2a71aa9b9")
     ]
 )
