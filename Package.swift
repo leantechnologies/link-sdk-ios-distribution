57721d97b6c211508ce1e2ea7054962cd987cca5
seb <98971553+seb-lean@users.noreply.github.com>
2022-07-01 14:57:06 +0100

    Sdk 303 release 1.5.0 ios sdk (#10)
    * SDK-303 feat: added 1.5.0, updated readme to include country and redirect urls

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/1.4.0/LeanSDK.xcframework-1.4.0.zip", checksum: "e1c319dd7a9d741b9a4905bcc91c0f2f183c9f06673df645692fb698b8bc57bb")
+        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/1.5.0/LeanSDK.xcframework-1.5.0.zip", checksum: "fc78695379d66a654ed5687197a3709a00276c3cb6f3ae768b9bb9a2a71aa9b9")
     ]
 )
