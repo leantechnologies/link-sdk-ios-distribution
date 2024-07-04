67000225b1a0a573b5bb6dd579184aca3ff3b39a
Kingdom Orjiewuru <kingdom.orjiewuru@gmail.com>
2024-07-04 12:34:23 +0400

    Release v3.0.7

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/build.11/LeanSDK.xcframework-build.11.zip", checksum: "860c42daddfbd791fbad6e2b433edc0af750ec1a4bd125f8d2d2a3bcebddb071")
+        .binaryTarget(name: "LeanSDK", url: "https://cdn.leantech.me/link/sdk/ios/build.12/LeanSDK.xcframework-build.12.zip", checksum: "bc1cd8b2a7615558e2707ee6b31961b45db13a1e8648ce38bc8a9104f0895f81")
     ]
 )
